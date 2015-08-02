ad4ad20e332bcf9eb2b98a9fba87659554e48799
Kevin Scalzo <tornic@gmail.com>
2015-08-02 18:16:27 -0500

    Simplified solution

--- EulerProjectMath/numberToLetters.cs
+++ EulerProjectMath/numberToLetters.cs
@@ -11,18 +11,12 @@ namespace EulerProject.EulerProjectMath
 
         public static string letters(int number)
         {
-            /* input a number, return that number written out in letters
-             * 
-             * can chop off the last digit of a number by doing % 10
-             * can get the last 2 digits by % 100
-             * 
-             */            
+            // input a number, return that number written out in letters                          
             
             string[] oneToNineteen = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven",
-                                     "twelve", "thirteen", "forteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+                                     "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
 
-            string[] twentyToNinety = { "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
-
+            string[] twentyToNinety = { "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };           
 
             if (number >= 1 && number <= 19)
             {
@@ -30,170 +24,31 @@ namespace EulerProject.EulerProjectMath
 
             }
 
-            if (number >= 20 && number <=29)
-            {                                
-                return "Twenty" + letters(number % 10);
+            if (number >= 20 && number <= 99)
+            {
+                return twentyToNinety[(number / 10) - 2] + letters(number % 10);
 
             }
             
-            if (number >= 30 && number <= 39)
+            if (number >= 100 && number <= 999)
             {
-                return "Thirty" + letters(number % 10);
+                return letters(number / 100) + "Hundred" + And(number) + letters(number % 100);
 
             }
-
-            if (number >= 40 && number <= 49)
-            {
-                return "Forty" + letters(number % 10);
-
-            }
-
-            if (number >= 50 && number <= 59)
-            {
-                return "Fifty" + letters(number % 10);
-
-            }
-
-            if (number >= 60 && number <= 69)
-            {
-                return "Sixty" + letters(number % 10);
-
-            }
-
-            if (number >= 70 && number <= 79)
-            {
-                return "Seventy" + letters(number % 10);
-
-            }
-
-            if (number >= 80 && number <= 89)
-            {
-                return "Eighty" + letters(number % 10);
-
-            }
-            if (number >= 90 && number <= 99)
-            {
-                return "Ninety" + letters(number % 10);
-
-            }
-
-            if (number == 100)
-            {
-                return "OneHundred";
-
-            }
-
-            if (number > 100 && number <= 199)
-            {
-                return "OneHundredAnd" + letters(number % 100);
-
-            }
-
-            if (number == 200)
-            {
-                return "TwoHundred";
-
-            }
-
-            if (number > 200 && number <= 299)
-            {
-                return "TwoHundredAnd" + letters(number % 100);
-
-            }
-
-            if (number == 300)
-            {
-                return "ThreeHundred";
-
-            }
-
-            if (number > 300 && number <= 399)
-            {
-                return "ThreeHundredAnd" + letters(number % 100);
-
-            }
-
-            if (number == 400)
-            {
-                return "FourHundred";
-
-            }
-
-            if (number > 400 && number <= 499)
-            {
-                return "FourHundredAnd" + letters(number % 100);
-
-            }
-
-            if (number == 500)
-            {
-                return "FiveHundred";
-
-            }
-
-            if (number > 500 && number <= 599)
-            {
-                return "FiveHundredAnd" + letters(number % 100);
-
-            }
-
-            if (number == 600)
-            {
-                return "SixHundred";
-
-            }
-
-            if (number > 600 && number <= 699)
-            {
-                return "SixHundredAnd" + letters(number % 100);
-
-            }
-
-            if (number == 700)
-            {
-                return "SevenHundred";
-
-            }
-            if (number > 700 && number <= 799)
-            {
-                return "SevenHundredAnd" + letters(number % 100);
-
-            }
-
-            if (number == 800)
-            {
-                return "EightHundred";
-
-            }
-
-            if (number > 800 && number <= 899)
-            {
-                return "EightHundredAnd" + letters(number % 100);
-
-            }
-
-            if (number == 900)
-            {
-                return "NineHundred";
-
-            }
-
-            if (number > 900 && number <= 999)
-            {
-                return "NineHundredAnd" + letters(number % 100);
-
-            }
-
+            
             if (number == 1000)
             {
                 return "OneThousand";
 
-            }
-
-            
+            }            
             return "";
+        }
 
-
+        public static string And(int num)
+        {
+            if (num != 100 && num != 200 && num != 300 && num != 400 && num != 500 && num != 600 && num != 700 && num != 800 && num != 900)
+                return "and";
+            else return "";
         }
 
     }
