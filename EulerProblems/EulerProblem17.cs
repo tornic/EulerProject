5e87ffc4a221ff5d63fbb920c90acb752f8dc981
Kevin Scalzo <tornic@gmail.com>
2015-08-02 11:12:26 -0500

    problem 17

--- EulerProblems/EulerProblem17.cs
+++ EulerProblems/EulerProblem17.cs
@@ -16,189 +16,20 @@ namespace EulerProject.EulerProblems
 
         //NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two)
         //contains 23 letters and 115 (one hundred and fifteen) contains 20 letters.
-        //The use of "and" when writing out numbers is in compliance with British usage.
-
-        // three hundred and forty two
-        // seven hundred and forty two
-
-        /*
-         * one, two, three, four, five, six, seven, eight, nine, ten
-         * 
-         * eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen
-         * eightteen, nineteen
-         * 
-         * twenty, thirty, fourty, fifty, sixty, seventy, eighty, ninty
-         * 
-         * one-hundred, two-hundred
-         * 
-         * and
-         * 
-         * group 1: one, two, six, ten (count 1 multply by 4)
-         * group 2: three, seven, eight (count 1 multiply by 3)
-         * group 3: four, five, nine (count 1 multply by 3)
-         * 
-         * group 1: twenty, thirty, eighty, ninety (count 1 multiply by 4)
-         * group 2: forty, fifty, sixty (count 1 multiply by 5)
-         * group 3: seventy (count 1 multiply by 7)
-         * 
-         *  string twenty = "twenty";
-            string thirty = "thirty";
-            string forty = "forty";
-            string fifty = "fifty";
-            string sixty = "sixty";
-            string seventy = "seventy";
-            string eighty = "eighty";
-            string ninety = "ninety";
-
-         */
-
+        //The use of "and" when writing out numbers is in compliance with British usage.        
+         
         public static int solve()
         {
             
-            int answer = 0;
-
+            string answer = "";
             
-                answer = EulerProjectMath.numberToLetters.letters(1);
+            for (int i = 1; i <= 1000; i++ )
+            { 
+                answer += EulerProjectMath.numberToLetters.letters(i);
 
-            
-
-
-
-                /*
-                List<string> numbers = new List<string>();
-                List<string> singleDigits = new List<string>();
-                List<string> doubleDigits = new List<string>();
-
-                List<string> hundreds = new List<string>();
-
-                doubleDigits.Add("eleven");
-                doubleDigits.Add("twelve");
-                doubleDigits.Add("thirteen");
-                doubleDigits.Add("fourteen");
-                doubleDigits.Add("fifteen");
-                doubleDigits.Add("sixteen");
-                doubleDigits.Add("seventeen");
-                doubleDigits.Add("eighteen");
-                doubleDigits.Add("nineteen");
-
-                string one = "one";
-                string two = "two";
-                string three = "three";
-                string four = "four";
-                string five = "five";
-                string six = "six";
-                string seven = "seven";
-                string eight = "eight";
-                string nine = "nine";
-                string ten = "ten";
-
-                string twenty = "twenty";
-                string thirty = "thirty";
-                string forty = "forty";
-                string fifty = "fifty";
-                string sixty = "sixty";
-                string seventy = "seventy";
-                string eighty = "eighty";
-                string ninety = "ninety";
-
-                singleDigits.Add(one);
-                singleDigits.Add(two);
-                singleDigits.Add(three);
-                singleDigits.Add(four);
-                singleDigits.Add(five);
-                singleDigits.Add(six);
-                singleDigits.Add(seven);
-                singleDigits.Add(eight);
-                singleDigits.Add(nine);
-            
-
-                foreach (string num in singleDigits)
-                {
-                    numbers.Add(num);
-
-                }
-                numbers.Add(ten);
-
-                numbers.Add("eleven");
-                numbers.Add("twelve");
-                numbers.Add("thirteen");
-                numbers.Add("fourteen");
-                numbers.Add("fifteen");
-                numbers.Add("sixteen");
-                numbers.Add("seventeen");
-                numbers.Add("eighteen");
-                numbers.Add("nineteen");
-                numbers.Add(twenty);
-
-                foreach (string num in singleDigits)
-                {
-                    numbers.Add(twenty + num);
-
-                }
-                numbers.Add(thirty);
-
-                foreach (string num in singleDigits)
-                {
-                    numbers.Add(thirty + num);
-
-                }
-                numbers.Add(forty);
-
-                foreach (string num in singleDigits)
-                {
-                    numbers.Add(forty + num);
-
-                }
-                numbers.Add(fifty);
-
-                foreach (string num in singleDigits)
-                {
-                    numbers.Add(fifty + num);
-
-                }
-                numbers.Add(sixty);
-
-                foreach (string num in singleDigits)
-                {
-                    numbers.Add(sixty + num);
-
-                }
-                numbers.Add(seventy);
-
-                foreach (string num in singleDigits)
-                {
-                    numbers.Add(seventy + num);
-
-                }
-                numbers.Add(eighty);
-
-                foreach (string num in singleDigits)
-                {
-                    numbers.Add(eighty + num);
-
-                }
-                numbers.Add(ninety);
-
-                foreach (string num in singleDigits)
-                {
-                    numbers.Add(ninety + num);
-
-                }
-
-                foreach (string num in singleDigits)
-                {
-                    hundreds.Add("100" + num);
-
-                }
-
-                foreach (string num in doubleDigits)
-                {
-                    hundreds.Add("100" + num);
-
-                }
-
-                */
-                return answer;
+            }
+               
+            return answer.Count();
         }
     }
 }
--- EulerProjectMath/numberToLetters.cs
+++ EulerProjectMath/numberToLetters.cs
@@ -9,27 +9,189 @@ namespace EulerProject.EulerProjectMath
     static class numberToLetters
     {
 
-        public static int letters(int number)
+        public static string letters(int number)
         {
             /* input a number, return that number written out in letters
              * 
              * can chop off the last digit of a number by doing % 10
+             * can get the last 2 digits by % 100
              * 
-             * 
-             */
-            string letters = "";
-
+             */            
+            
             string[] oneToNineteen = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven",
                                      "twelve", "thirteen", "forteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
 
+            string[] twentyToNinety = { "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
 
-            if (number >= 0 && number <= 19)
+
+            if (number >= 1 && number <= 19)
             {
-                return oneToNineteen[number].Length;
+                return oneToNineteen[number];
 
             }
 
-            return letters.Length;
+            if (number >= 20 && number <=29)
+            {                                
+                return "Twenty" + letters(number % 10);
+
+            }
+            
+            if (number >= 30 && number <= 39)
+            {
+                return "Thirty" + letters(number % 10);
+
+            }
+
+            if (number >= 40 && number <= 49)
+            {
+                return "Forty" + letters(number % 10);
+
+            }
+
+            if (number >= 50 && number <= 59)
+            {
+                return "Fifty" + letters(number % 10);
+
+            }
+
+            if (number >= 60 && number <= 69)
+            {
+                return "Sixty" + letters(number % 10);
+
+            }
+
+            if (number >= 70 && number <= 79)
+            {
+                return "Seventy" + letters(number % 10);
+
+            }
+
+            if (number >= 80 && number <= 89)
+            {
+                return "Eighty" + letters(number % 10);
+
+            }
+            if (number >= 90 && number <= 99)
+            {
+                return "Ninety" + letters(number % 10);
+
+            }
+
+            if (number == 100)
+            {
+                return "OneHundred";
+
+            }
+
+            if (number > 100 && number <= 199)
+            {
+                return "OneHundredAnd" + letters(number % 100);
+
+            }
+
+            if (number == 200)
+            {
+                return "TwoHundred";
+
+            }
+
+            if (number > 200 && number <= 299)
+            {
+                return "TwoHundredAnd" + letters(number % 100);
+
+            }
+
+            if (number == 300)
+            {
+                return "ThreeHundred";
+
+            }
+
+            if (number > 300 && number <= 399)
+            {
+                return "ThreeHundredAnd" + letters(number % 100);
+
+            }
+
+            if (number == 400)
+            {
+                return "FourHundred";
+
+            }
+
+            if (number > 400 && number <= 499)
+            {
+                return "FourHundredAnd" + letters(number % 100);
+
+            }
+
+            if (number == 500)
+            {
+                return "FiveHundred";
+
+            }
+
+            if (number > 500 && number <= 599)
+            {
+                return "FiveHundredAnd" + letters(number % 100);
+
+            }
+
+            if (number == 600)
+            {
+                return "SixHundred";
+
+            }
+
+            if (number > 600 && number <= 699)
+            {
+                return "SixHundredAnd" + letters(number % 100);
+
+            }
+
+            if (number == 700)
+            {
+                return "SevenHundred";
+
+            }
+            if (number > 700 && number <= 799)
+            {
+                return "SevenHundredAnd" + letters(number % 100);
+
+            }
+
+            if (number == 800)
+            {
+                return "EightHundred";
+
+            }
+
+            if (number > 800 && number <= 899)
+            {
+                return "EightHundredAnd" + letters(number % 100);
+
+            }
+
+            if (number == 900)
+            {
+                return "NineHundred";
+
+            }
+
+            if (number > 900 && number <= 999)
+            {
+                return "NineHundredAnd" + letters(number % 100);
+
+            }
+
+            if (number == 1000)
+            {
+                return "OneThousand";
+
+            }
+
+            
+            return "";
 
 
         }
