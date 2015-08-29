3e0e357568ca5e37a5ab0cf17a7750a5e8f44c9b
Josh Rickard <josh.rickard@gmail.com>
2015-08-29 11:17:48 -0500

    add problem 67 with notes on potential optimization

---
+++ EulerProblems/EulerProblem67.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProject.EulerProblems
+{
+    public static class EulerProblem67
+    {
+        /*
+         * 
+         * Potential optimizations
+         *      For each row assume remaining rows are all max value (i.e. 99) and if the sum of (remaining rows * 99) < bestTotal
+         *      then we can eliminate this path.
+         *      
+         * 
+         * 
+         * 
+         * 
+         * 
+         **/
+    }
+}
